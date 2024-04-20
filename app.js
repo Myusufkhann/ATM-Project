275a17c017bbd40cb8742bf78b16ca15eee29134
Myusufkhann <160284864+Myusufkhann@users.noreply.github.com>
2024-04-20 14:46:03 -0700

    ATM Project

---
+++ app.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { type: "input", name: "userId", message: "Please enter your ID" },
+    { type: "number", name: "userPin", message: "Please enter your PIN" },
+    { type: "list", name: "accountType", message: "Please select your account type",
+        choices: ["savings", "current"] },
+    { type: "list", name: "transactionType",
+        message: "Please select your transaction type",
+        choices: ["deposit", "withdrawal"], when(answers) { return answers.accountType; }, },
+    { type: "list", name: "amount", choices: [1000, 5000, 10000, 100000],
+        message: "select amount", when(answers) { return answers.transactionType == "deposit"; } },
+    { type: "list", name: "amount", choices: [1000, 5000, 10000, 100000],
+        message: "select amount", when(answers) { return answers.transactionType == "withdrawal"; }, },
+]);
+if (answer.userId && answer.userPin) {
+    const balance = Math.floor(Math.random() * 100000);
+    console.log("Previous balance", balance);
+    const enteredAmount = answer.amount;
+    if (balance >= enteredAmount) {
+        const remainingBalance = balance - enteredAmount;
+        console.log("your Remaining balance is", remainingBalance);
+    }
+    else {
+        console.log("insufficient balance");
+    }
+}
